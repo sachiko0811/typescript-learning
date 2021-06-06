4c80524d805d89b54bcf72572d9abab2fc90b45f
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-06 15:38:16 -0700

    Tuple type

--- app.js
+++ app.js
@@ -9,8 +9,12 @@
 var person = {
     name: 'Kipu',
     age: 25,
-    hobbies: ['Cooking', 'Workout']
+    hobbies: ['Cooking', 'Workout'],
+    role: [2, 'author']
 };
+// person.role.push('admin'); // error
+// person.role[1] = 10; // error
+// person.role = [0, 'admin', 'user']; // error
 var favoriteActivities;
 favoriteActivities = ['Workout'];
 // favoriteActivities = 'Workout'; // error
@@ -18,4 +22,5 @@ console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) // error cuz string doesn't support .map()
 }
--- app.ts
+++ app.ts
@@ -8,11 +8,22 @@
 //   age: 25,
 // }
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Kipu',
   age: 25,
-  hobbies: ['Cooking', 'Workout']
+  hobbies: ['Cooking', 'Workout'],
+  role: [2, 'author'],
 };
+
+// person.role.push('admin'); // error
+// person.role[1] = 10; // error
+// person.role = [0, 'admin', 'user']; // error
+
 
 let favoriteActivities: string[];
 favoriteActivities = ['Workout'];
