7f45231f65e950f712c74c92a7229fd8ef0ef22a
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-06 14:37:40 -0700

    app.ts

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 5;
+var num2 = 2.8;
+var res = add(num1, num2);
+console.log(res);
---
+++ app.ts
@@ -0,0 +1,13 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+
+const res = add(num1, num2);
+console.log(res);
+
+// const num1 = "5";
+// const num2 = 2.5;
+// res = 52.5
