b64878468958274d195cd9d3e80bf4832ef6474c
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-06 15:47:52 -0700

    Enum type

--- app.js
+++ app.js
@@ -6,15 +6,35 @@
 //   name: 'Kipu',
 //   age: 25,
 // }
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Kipu',
+//   age: 25,
+//   hobbies: ['Cooking', 'Workout'],
+//   role: [2, 'author'],
+// };
+// person.role.push('admin'); // error
+// person.role[1] = 10; // error
+// person.role = [0, 'admin', 'user']; // error
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Kipu',
     age: 25,
     hobbies: ['Cooking', 'Workout'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
-// person.role.push('admin'); // error
-// person.role[1] = 10; // error
-// person.role = [0, 'admin', 'user']; // error
 var favoriteActivities;
 favoriteActivities = ['Workout'];
 // favoriteActivities = 'Workout'; // error
@@ -23,4 +43,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()) // error cuz string doesn't support .map()
+}
+if (person.role === Role.ADMIN) {
+    console.log('Admin user');
 }
--- app.ts
+++ app.ts
@@ -8,22 +8,38 @@
 //   age: 25,
 // }
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: 'Kipu',
-  age: 25,
-  hobbies: ['Cooking', 'Workout'],
-  role: [2, 'author'],
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Kipu',
+//   age: 25,
+//   hobbies: ['Cooking', 'Workout'],
+//   role: [2, 'author'],
+// };
 
 // person.role.push('admin'); // error
 // person.role[1] = 10; // error
 // person.role = [0, 'admin', 'user']; // error
 
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN, 
+  READ_ONLY, 
+  AUTHOR
+}
+
+const person = {
+  name: 'Kipu',
+  age: 25,
+  hobbies: ['Cooking', 'Workout'],
+  role: Role.ADMIN,
+};
 
 let favoriteActivities: string[];
 favoriteActivities = ['Workout'];
@@ -34,4 +50,8 @@ console.log(person);
 for(const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
   // console.log(hobby.map()) // error cuz string doesn't support .map()
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('Admin user');
 }
