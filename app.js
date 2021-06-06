3a41fe35f717bc7c46dce70d81602b08971a080d
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-06 14:57:57 -0700

    number string boolean

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    // console.log(typeof n1);
+    // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error("It's not correct!");
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+        // console.log(phrase + n1 + n2);  // when the phrase is string, then the answer will be string(52.8)
+    }
+    else {
+        return result;
+    }
 }
 var num1 = 5;
 var num2 = 2.8;
-var res = add(num1, num2);
-console.log(res);
+var printResult = true;
+var resultPhrase = 'Result: ';
+var res = add(num1, num2, printResult, resultPhrase);
+// console.log(res);
+// const num1 = "5";
+// const num2 = 2.5;
+// res = 52.5
--- app.ts
+++ app.ts
@@ -1,12 +1,24 @@
-function add(n1, n2) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // console.log(typeof n1);
+  // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error("It's not correct!");
+  // }
+  const result = n1 + n2;
+  if(showResult) {
+    console.log(phrase + result);
+    // console.log(phrase + n1 + n2);  // when the phrase is string, then the answer will be string(52.8)
+  } else {
+    return result;
+  }
 }
 
 const num1 = 5;
 const num2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result: ';
 
-const res = add(num1, num2);
-console.log(res);
+const res = add(num1, num2, printResult, resultPhrase);
+// console.log(res);
 
 // const num1 = "5";
 // const num2 = 2.5;
