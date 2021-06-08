b7fc3d86d90ae422824e086fe1e4f9e247ac53b4
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-08 11:04:43 -0700

    Union type

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 //   const result = n1 + n2;
 //   return result;
 // }
+// Union type
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + " " + input2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Eve', 'Yoko');
+console.log(combinedNames);
