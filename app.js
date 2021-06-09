2dd2ef1b7e95a5602378eb7caa46eaecce074cdb
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-09 15:34:05 -0700

    function type

--- app.js
+++ app.js
@@ -1,71 +1,12 @@
-// function add(n1: number, n2: number) { // In this case, you can just use "number" parameters
-//   const result = n1 + n2;
-//   return result;
-// }
-// Union type
-// function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
-//   let result;
-//   if ((typeof input1 === 'number' && typeof input2 === 'number') || 
-//   resultConversion === 'as-number' 
-//   ) {
-//     // result = input1 + input2;
-//     result = +input1 + +input2; // convert to number
-//   } else {
-//     result = input1.toString() + " " + input2.toString();
-//   }
-//   return result;
-//   // if (resultConversion === 'as-number') {
-//   //   return +result; // or parseFloat(result)
-//   // } else {
-//   //   return result.toString();
-//   // }
-// }
-// const combinedAges = combine(30, 26, 'as-number');
-// console.log(combinedAges);
-// const combinedStringAges = combine('30', '26', 'as-number');
-// console.log(combinedStringAges);
-// const combinedNames = combine('Eve', 'Yoko', 'as-text');
-// console.log(combinedNames);
-// alias(custom)
-// type Combinable = number | string;
-// type ConversionDescriptor = 'as-number' | 'as-text';
-// function combine(
-//   input1: Combinable,
-//   input2: Combinable,
-//   resultConversion: 'as-number' | 'as-text',
-// ) {
-//   let result;
-//   if (
-//     (typeof input1 === "number" && typeof input2 === "number") ||
-//     resultConversion === "as-number"
-//   ) {
-//     result = +input1 + +input2;
-//   } else {
-//     result = input1.toString() + input2.toString();
-//   }
-//   return result;
-// }
-// const combinedAges = combine(30, 26, 'as-number');
-// console.log(combinedAges);
-// const combinedStringAges = combine('30', '26', 'as-number');
-// console.log(combinedStringAges);
-// const combinedNames = combine('Eve', 'Yoko', 'as-text');
-// console.log(combinedNames);
-// alias (Obj)
-// type User = { name: string; age: number};
-var u1 = { name: 'Max', age: 30 };
-// no alias version
-// function greet(user: { name: string; age: number;}) {
-//   console.log(`Hi, I am ` + user.name);
-// }
-// function isOlder(user: { name: string; age: number}, checkAge: number) {
-//   return console.log(checkAge > user.age);
-// }
-greet(u1);
-isOlder(u1, 25);
-function greet(user) {
-    console.log("Hi, I am " + user.name);
+function add(num1, num2) {
+    return num1 + num2;
 }
-function isOlder(user, checkAge) {
-    console.log(checkAge > user.age);
+function printResult(num) {
+    console.log('Result: ' + num);
 }
+// let combineValues;  // this valuable is any type so you can store not only any but also others
+// let combineValues: Function; // not enough
+var combineValues;
+combineValues = add; // store add function to combineValues(valuable)
+console.log(combineValues(1, 1)); // run the add function through combineValues
+printResult(add(5, 12));
