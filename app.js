e90946f270a2b31a4cd4a4ac40685135b687b032
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-09 15:41:29 -0700

    function type and callback

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ function add(num1, num2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var res = n1 + n2;
+    cb(res);
+}
+addAndHandle(10, 20, function (res) {
+    console.log(res);
+});
 // let combineValues;  // this valuable is any type so you can store not only any but also others
 // let combineValues: Function; // not enough
 var combineValues;
--- app.ts
+++ app.ts
@@ -6,6 +6,15 @@ function printResult(num: number) {
   console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const res = n1 + n2;
+  cb(res);
+}
+
+addAndHandle(10, 20, (res) => {
+  console.log(res);
+});
+
 // let combineValues;  // this valuable is any type so you can store not only any but also others
 // let combineValues: Function; // not enough
 let combineValues: (a: number, b:number) => number;
