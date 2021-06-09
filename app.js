1829666e7b91975412c649dec241d68c0da19387
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-06-09 15:51:05 -0700

    unknown type

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-function add(num1, num2) {
-    return num1 + num2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput; // error
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var res = n1 + n2;
-    cb(res);
-}
-addAndHandle(10, 20, function (res) {
-    console.log(res);
-});
-// let combineValues;  // this valuable is any type so you can store not only any but also others
-// let combineValues: Function; // not enough
-var combineValues;
-combineValues = add; // store add function to combineValues(valuable)
-console.log(combineValues(1, 1)); // run the add function through combineValues
-printResult(add(5, 12));
---
+++ app.ts
@@ -0,0 +1,11 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+// userName = userInput; // error
+
+if(typeof userInput === 'string') {
+  userName = userInput;
+}
